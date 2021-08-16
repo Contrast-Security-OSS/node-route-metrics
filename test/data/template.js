e571de61088a856404a3d7479271131bc442fe25
Bruce A. MacNaughton <bruce.macnaughton@contrastsecurity.com>
2021-08-15 18:21:56 -0700

    add noecho params route

--- test/data/template.js
+++ test/data/template.js
@@ -9,6 +9,7 @@ module.exports = {
     title: 'this is my test server'
   },
   routes: [
+    {name: 'noecho (ALL PARAMS)', method: 'POST', regex: /^\/noecho(\?.+)?/},
     {name: 'GET /', method: 'GET', pattern: '/'},
     {name: '/wait/:time', method: 'GET', regex: /^\/wait\/.+/},
     {name: '/wait/10', method: 'GET', regex: /^\/wait\/10/},
